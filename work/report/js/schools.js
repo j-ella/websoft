62ff25b2889aeacd80d672abde855e8ab8e86036
j-ella <j.ella@outlook.com>
2020-01-28 20:22:39 +0100

    alotta csssss

---
+++ work/report/js/schools.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+//    fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081')
+    fetch('data/1081.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
+
+    console.log('Sandbox MEGA is ready!');
+})();
