41a82a8c45f97c4d3ed508ed893376cf8b9888e5
j-ella <j.ella@outlook.com>
2020-02-03 10:36:08 +0100

    php added

---
+++ work/php/js/duck.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function () {
+
+    var element = document.getElementById("duck"); 
+    element.addEventListener("click", function() {
+    element.style.top = "200px";
+    });
+
+})();
